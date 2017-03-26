864120f06fbcb6eb14c0b3abdbbeab5cf20fbc5f
Aik Siang <aiksiang.ang@grabtaxi.com>
2017-03-27 00:06:46 +0800

    Dressabelle Name and Price

--- crawler/dressabelle.go
+++ crawler/dressabelle.go
@@ -11,7 +11,14 @@ import (
 )
 
 var dressabelleSeeds = []string{
-	"https://www.dressabelle.com.sg",
+	// "https://www.dressabelle.com.sg",
+	"https://www.dressabelle.com.sg/dsb/new-arrivals/asymmetrical-top-navy-detail.html",
+}
+
+var dressabelleBlacklists = []string{
+	"https://www.dressabelle.com.sg/create-an-account.html",
+	"https://www.dressabelle.com.sg/userarea.html",
+	"https://www.dressabelle.com.sg/cart.html",
 }
 
 // BBDressabelleExtender .
@@ -19,16 +26,24 @@ type BBDressabelleExtender struct {
 	gocrawl.DefaultExtender
 }
 
+// Visit .
 func (x *BBDressabelleExtender) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
 	// Use the goquery document or res.Body to manipulate the data
 	fmt.Println(doc.Url)
+	name := parseName(doc)
+	fmt.Println(name)
+	price := parsePrice(doc)
+	fmt.Println(price)
+
 	return nil, true
 }
 
+// Filter .
 func (x *BBDressabelleExtender) Filter(ctx *gocrawl.URLContext, isVisited bool) bool {
 	return !isVisited
 }
 
+// CrawlDressabelle .
 func CrawlDressabelle(wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Printf("Crawling Dressabelle...")
@@ -37,4 +52,32 @@ func CrawlDressabelle(wg *sync.WaitGroup) {
 	opts.CrawlDelay = 100 * time.Millisecond
 	crawler := gocrawl.NewCrawlerWithOptions(opts)
 	crawler.Run(dressabelleSeeds)
+}
+
+func parseName(doc *goquery.Document) string {
+	s := doc.Find("h1").FilterFunction(
+		func(_ int, s *goquery.Selection) bool {
+			propName, exists := s.Attr("itemprop")
+			if exists && propName == "name" {
+				return true
+			}
+			return false
+		})
+	return s.Text()
+}
+
+func parsePrice(doc *goquery.Document) string {
+	s := doc.Find("meta").FilterFunction(
+		func(i int, s *goquery.Selection) bool {
+			propName, exists := s.Attr("itemprop")
+			if exists && propName == "price" {
+				return true
+			}
+			return false
+		})
+	price, exists := s.Attr("content")
+	if !exists {
+		return ""
+	}
+	return price
 }
